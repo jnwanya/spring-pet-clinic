c41369953493ad953e295e733d83d0048fe64e9a
Nwanya Justin <nwanyajustin@gmail.com>
2018-08-26 21:30:03 +0100

    Added Specialities entity and updated Vet

---
+++ pet-clinic-data/src/main/java/com/jnwanya/springpetclinic/model/Speciality.java
@@ -0,0 +1,18 @@
+package com.jnwanya.springpetclinic.model;
+
+/**
+ * Created by Jnwanya on
+ * Sun, 26 Aug, 2018.
+ */
+public class Speciality extends BaseEntity {
+
+    private String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
--- pet-clinic-data/src/main/java/com/jnwanya/springpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/com/jnwanya/springpetclinic/model/Vet.java
@@ -1,8 +1,20 @@
 package com.jnwanya.springpetclinic.model;
 
+import java.util.Set;
+
 /**
  * Created by Jnwanya on
  * Thu, 16 Aug, 2018.
  */
 public class Vet extends Person {
+
+    private Set<Speciality> specialities;
+
+    public Set<Speciality> getSpecialities() {
+        return specialities;
+    }
+
+    public void setSpecialities(Set<Speciality> specialities) {
+        this.specialities = specialities;
+    }
 }
